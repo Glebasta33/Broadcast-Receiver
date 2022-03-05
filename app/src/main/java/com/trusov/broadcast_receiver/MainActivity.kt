6bc18667e3c28dda5499a5416502e474670ece26
Glebasta333 <gleb.trusow@yandex.ru>
2022-03-05 15:45:26 +0300

    add MyReceiver

--- app/src/main/java/com/trusov/broadcast_receiver/MainActivity.kt
+++ app/src/main/java/com/trusov/broadcast_receiver/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.trusov.broadcast_receiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(receiver, intentFilter)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
 }
