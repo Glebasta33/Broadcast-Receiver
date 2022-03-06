8d1390de66379bd5dfacea7cb7c8bbd84b944289
Glebasta333 <gleb.trusow@yandex.ru>
2022-03-06 13:46:29 +0300

    add broadcast sending

--- app/src/main/java/com/trusov/broadcast_receiver/MainActivity.kt
+++ app/src/main/java/com/trusov/broadcast_receiver/MainActivity.kt
@@ -4,17 +4,27 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val intent = Intent().apply {
+                action = MyReceiver.ACTION_SEND_BROADCAST
+                putExtra(MyReceiver.EXTRA_TEXT, "Broadcast sent ${counter++}")
+            }
+            sendBroadcast(intent)
+        }
         val intentFilter = IntentFilter().apply {
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             addAction(Intent.ACTION_SCREEN_OFF)
+            addAction(MyReceiver.ACTION_SEND_BROADCAST)
         }
         registerReceiver(receiver, intentFilter)
     }
@@ -23,5 +33,4 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         unregisterReceiver(receiver)
     }
-
 }
--- app/src/main/java/com/trusov/broadcast_receiver/MyReceiver.kt
+++ app/src/main/java/com/trusov/broadcast_receiver/MyReceiver.kt
@@ -7,20 +7,29 @@ import android.widget.Toast
 
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        when (intent?.action) {
+        when(intent?.action) {
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
-                val turnOn = intent.getBooleanExtra("state", false)
-                val text = if (turnOn) {
-                    "Airplane mode activated"
+                val state = intent.getBooleanExtra("state", false)
+                val text = if (state) {
+                    "Airplane mode"
                 } else {
-                    "Airplane mode unactivated"
+                    "Unactivated"
                 }
                 Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
             }
             Intent.ACTION_SCREEN_OFF -> {
                 Toast.makeText(context, "Screen off", Toast.LENGTH_SHORT).show()
             }
+            ACTION_SEND_BROADCAST -> {
+                val text = intent.getStringExtra(EXTRA_TEXT)
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    companion object {
+        const val ACTION_SEND_BROADCAST = "send_broadcast"
+        const val EXTRA_TEXT = "extra"
     }
 
 }
